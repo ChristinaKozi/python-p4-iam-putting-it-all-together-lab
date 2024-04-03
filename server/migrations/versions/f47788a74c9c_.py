c0a320732519e99c04bf6ab6a69384b78aaf2b3d
Christina Kozanian <christinakozanian@gmail.com>
2024-04-03 13:17:13 -0700

    models test pass

---
+++ server/migrations/versions/f47788a74c9c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f47788a74c9c
+Revises: 
+Create Date: 2024-04-03 12:37:33.046304
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f47788a74c9c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
